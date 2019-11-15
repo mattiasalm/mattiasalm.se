bba68e323fb4752174164b2d6b4cf12f31f7d105
Mattias Alm <mattias.alm@stendahls.se>
2019-11-15 14:08:49 +0100

    Add build script

--- build.sh
+++ build.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 
-minify src/styles/*.css
+rm -rf dist
+mkdir -p dist/styles
+mkdir -p dist/scripts
+minify src/index.html > dist/index.html
+minify src/styles/*.css > dist/styles/main.css
+minify src/scripts/*.js > dist/scripts/main.js
+cp -R src/content dist/content
