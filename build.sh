615eeb3015cd813d763f51051beccfbd89ac1a1b
Mattias Alm <mattias.alm@stendahls.se>
2019-11-25 10:14:38 +0100

    Update scripts

--- build.sh
+++ build.sh
@@ -1,22 +1,31 @@
 #!/usr/bin/env bash
 
-if ! hash minify 2>/dev/null; then
-    echo -e "\e[1m\e[31m minify not installed\n\n run \e[0m\e[7m\e[1m npm i -g minify \e[0m"
-    exit 1
-fi
+trimStr() {
+    while read data; do
+        echo $data | sed -e 's/^[ \t]*//g' -e ':a;N;$!ba;s/\n//g' | tr -d '\n\r'
+    done
+}
 
-if ! hash terser 2>/dev/null; then
-    echo -e "\e[1m\e[31m terser not installed\n\n run \e[0m\e[7m\e[1m npm i -g terser \e[0m"
-    exit 1
-fi
-start=$(perl -MTime::HiRes=time -e 'printf "%.9f\n", time')
 echo -en "\e[1m\e[32m Building...\e[0m"
+
+start=$(perl -MTime::HiRes=time -e 'printf "%.9f\n", time')
+
 rm -rf dist
 mkdir -p dist
-postcss src/index.html --syntax postcss-html -u autoprefixer cssnano postcss-css-variables --no-map -o dist/index.html
+
 cat src/styles/*.css | postcss -u autoprefixer cssnano postcss-css-variables --no-map -o dist/main.min.css
 terser src/scripts/*.js -o dist/main.min.js -c -m --mangle-props 'keep_quoted'
-cp -R src/content dist/content
+
+mkdir -p dist/content
+for file in src/content/*; do
+    local fileName=$(basename "$file")
+    echo $(cat "$file" | trimStr) > dist/content/$fileName
+done
+
+content=$(<dist/content/index.html)
+echo $(cat src/index.html | postcss --syntax postcss-html -u autoprefixer cssnano postcss-css-variables --no-map | trimStr | sed 's|<div id="content">|&'$content'|g') > dist/index.html
+
 end=$(perl -MTime::HiRes=time -e 'printf "%.9f\n", time')
 timing=$(printf "%0.2f\n" $((end-start)))
+
 echo -e "\e[1m\e[32mdone!\e[0m ("$timing"s)"
--- serve.sh
+++ serve.sh
@@ -1,15 +1,5 @@
 #!/usr/bin/env bash
 
-if ! hash superstatic 2>/dev/null; then
-    echo -e "\e[1m\e[31m superstatic not installed\n\n run \e[0m\e[7m\e[1m npm i -g superstatic \e[0m"
-    exit 1
-fi
-
-if ! hash onchange 2>/dev/null; then
-    echo -e "\e[1m\e[31m onchange not installed\n\n run \e[0m\e[7m\e[1m npm i -g onchange \e[0m"
-    exit 1
-fi
-
 zsh build.sh || exit 1 &&
 echo -e "\e[1m\e[33m Watching folders for changes...\e[0m" &
 onchange -d 1000 'src/**/*.*' -- zsh build.sh &
