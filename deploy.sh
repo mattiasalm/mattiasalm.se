e021d19c5777f34592c2bb9e5ae127d4688b7767
Mattias Alm <mattiasalm@gmail.com>
2020-03-02 15:26:36 +0100

    Introduce rollup.js for build & bundle

--- deploy.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-echo -e "\e[1m\e[33m Deploying to Zeit production...\e[0m"
-now --prod
---
+++ rollup.config.js
@@ -0,0 +1,79 @@
+import { terser } from 'rollup-plugin-terser';
+import rollupPostcss from 'rollup-plugin-postcss';
+import postcss from 'postcss';
+import atImport from 'postcss-import';
+import autoprefixer from 'autoprefixer';
+import postcssVariables from 'postcss-css-variables';
+import clean from 'postcss-clean';
+import CleanCSS from 'clean-css';
+import copy from 'rollup-plugin-copy';
+import { minify } from 'html-minifier-terser';
+
+const developmentMode = process.env.NODE_ENV === 'development';
+
+const copyTransform = contents => {
+  if (developmentMode) {
+    return contents.toString();
+  }
+
+  const processOptions = {
+    from: undefined,
+    map: false,
+  }
+
+  const htmlMinifyOptions = {
+    collapseInlineTagWhitespace: true,
+    collapseWhitespace: true,
+    conservativeCollapse: true,
+    minifyCSS: text => {
+      const processed = postcss([
+        autoprefixer,
+        postcssVariables
+      ]).process(text, processOptions).css;
+      return new CleanCSS().minify(processed).styles
+    },
+  }
+
+  return minify(contents.toString(), htmlMinifyOptions);
+}
+
+export default {
+  input: './src/main.js',
+  output: {
+    file: './public/main.min.js',
+    format: 'iife',
+  },
+  plugins: [
+    copy({
+      targets: [
+        { src: 'src/content/**/*',
+          dest: 'public/content',
+          transform: copyTransform,
+        },
+        { src: 'src/index.html',
+          dest: 'public',
+          transform: copyTransform,
+        }
+      ],
+    }),
+    rollupPostcss({
+      extract: true,
+      plugins: [
+        atImport(),
+        autoprefixer(),
+        !developmentMode && postcssVariables(),
+        !developmentMode && clean(),
+      ],
+    }),
+    !developmentMode && terser({
+      compress: {
+        module: true,
+      },
+      mangle: {
+        properties: {
+          keep_quoted: true,
+        },
+      },
+    }),
+  ],
+};
