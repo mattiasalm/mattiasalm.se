7394242db22cbfca974530daa9b7399769da5231
Mattias Alm <mattiasalm@gmail.com>
2020-09-09 22:30:22 +0200

    change color

--- src/app.config.js
+++ src/app.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   name: 'Mattias Alm',
   iconTextColor: 'rgba(255, 255, 255, 0.9)',
-  backgroundColor: '#f45734',
-  themeColor: '#f45734',
+  backgroundColor: '#e2532d',
+  themeColor: '#e2532d',
 };
